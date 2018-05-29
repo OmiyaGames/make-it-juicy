8e44e861c3e97b1c136ff4437be79e6099c6de72
Taro Omiya <taro.omiya@omiyagames.com>
2018-05-29 17:47:04 -0400

    Fixing the achievements causing the game to crash.

--- Assets/JuicyPresentation/Scripts/ScrollBackground.cs
+++ Assets/JuicyPresentation/Scripts/ScrollBackground.cs
@@ -4,16 +4,16 @@ using System.Collections;
 public class ScrollBackground : MonoBehaviour
 {
     public Transform flappyBird;
-	public float scrollSpeed = 5f;
+    public float scrollSpeed = 5f;
     public int incrementScore = 1;
     [Header("Background")]
-	public Transform[] background;
-	public int backgroundAt0 = 0;
+    public Transform[] background;
+    public int backgroundAt0 = 0;
     public Vector3 shiftBackgroundBy = new Vector3();
     public bool updateBackground = false;
     [Header("Foreground")]
-	public Rigidbody2D[] foreground;
-	public int foregroundAt0 = 1;
+    public Rigidbody2D[] foreground;
+    public int foregroundAt0 = 1;
     public Vector2 shiftForegroundBy = new Vector2();
     public bool updateForeground = false;
     [Header("Obstacles")]
@@ -42,9 +42,9 @@ public class ScrollBackground : MonoBehaviour
     public float increaseSpeedBy = 0.1f;
     public int increaseSpeedAfterLevels = 5;
 
-	int index, debrisIndex, diamondsIndex, levels = 1;
-	Vector3 left = new Vector3(-1, 0, 0);
-	Vector2 cache;
+    int index, debrisIndex, diamondsIndex, levels = 1;
+    Vector3 left = new Vector3(-1, 0, 0);
+    Vector2 cache;
     Vector3 shiftObstaclesBy3;
     float flashStarted = -1f;
     int diamondPlacement = 0;
@@ -55,60 +55,60 @@ public class ScrollBackground : MonoBehaviour
         diamondPlacement = 0;
         levels = 1;
         obstacle[0].position = PositionObstacle(obstacle[obstacle.Length - 1].position + shiftObstacleBy);
-        for(index = 0; index < obstacle.Length; ++index)
+        for (index = 0; index < obstacle.Length; ++index)
         {
             obstacle[index].position = PositionObstacle(obstacle[index].position);
         }
-        if((diamonds != null) && (diamonds.Length > 0))
+        if ((diamonds != null) && (diamonds.Length > 0))
         {
             PositionDiamond(obstacle[0].position);
         }
         shiftObstaclesBy3 = shiftObstacleBy;
-        if(backgroundPartiles != null)
+        if (backgroundPartiles != null)
         {
             backgroundPartiles.GetComponent<Renderer>().sortingLayerID = matchOrder.sortingLayerID;
             backgroundPartiles.GetComponent<Renderer>().sortingOrder = matchOrder.sortingOrder + 1;
         }
     }
 
-	// Update is called once per frame
-	void Update ()
-	{
-        if(enabled == true)
+    // Update is called once per frame
+    void Update()
+    {
+        if (enabled == true)
         {
-            if(updateBackground == true)
-    		{
-    			// Translate all the elements
-    			left.x = scrollSpeed * Time.deltaTime * -1f;
-    			for(index = 0; index < background.Length; ++index)
-    			{
-    				background[index].Translate(left);
-    			}
-
-    			// Sort the list
-    			if(background[backgroundAt0 + 1].position.x < 0)
-    			{	
-                    background[0].position = background[background.Length - 1].position + shiftBackgroundBy;
-    				ReSortArray<Transform>(background);
-    			}
-    		}
-            if((debris.Length > 0) && (flappyBird != null))
+            if (updateBackground == true)
             {
-                for(index = 0; index < (obstacleAt0 + 1); ++index)
+                // Translate all the elements
+                left.x = scrollSpeed * Time.deltaTime * -1f;
+                for (index = 0; index < background.Length; ++index)
                 {
-                    if((obstacle[index].gameObject.activeSelf == true) && (obstacle[index].position.x < flappyBird.position.x))
+                    background[index].Translate(left);
+                }
+
+                // Sort the list
+                if (background[backgroundAt0 + 1].position.x < 0)
+                {
+                    background[0].position = background[background.Length - 1].position + shiftBackgroundBy;
+                    ReSortArray<Transform>(background);
+                }
+            }
+            if ((debris.Length > 0) && (flappyBird != null))
+            {
+                for (index = 0; index < (obstacleAt0 + 1); ++index)
+                {
+                    if ((obstacle[index].gameObject.activeSelf == true) && (obstacle[index].position.x < flappyBird.position.x))
                     {
                         obstacle[index].gameObject.SetActive(false);
                         FlappyBirdController.CurrentScore += incrementScore;
-                        if(increaseDifficulty == true)
+                        if (increaseDifficulty == true)
                         {
-                            if((levels % increaseSpeedAfterLevels) == 0)
+                            if ((levels % increaseSpeedAfterLevels) == 0)
                             {
                                 scrollSpeed += increaseSpeedBy;
                             }
                             ++levels;
                         }
-                        if(rockFlash == null)
+                        if (rockFlash == null)
                         {
                             AnimateRocks(obstacle[index].position);
                         }
@@ -120,7 +120,7 @@ public class ScrollBackground : MonoBehaviour
                     }
                 }
             }
-            if((rockFlash != null) && (rockFlash.activeSelf == true) && ((Time.unscaledTime - flashStarted) > pauseSeconds))
+            if ((rockFlash != null) && (rockFlash.activeSelf == true) && ((Time.unscaledTime - flashStarted) > pauseSeconds))
             {
                 // Hide the flash
                 rockFlash.SetActive(false);
@@ -133,17 +133,17 @@ public class ScrollBackground : MonoBehaviour
                 AnimateRocks(rockFlash.transform.position);
             }
         }
-	}
+    }
 
     void FixedUpdate()
     {
-        if(enabled == true)
+        if (enabled == true)
         {
             left.x = scrollSpeed * Time.deltaTime * -1f;
-            if(updateForeground == true)
+            if (updateForeground == true)
             {
                 // Translate all the elements
-                for(index = 0; index < foreground.Length; ++index)
+                for (index = 0; index < foreground.Length; ++index)
                 {
                     cache = foreground[index].position;
                     cache.x += left.x;
@@ -151,25 +151,25 @@ public class ScrollBackground : MonoBehaviour
                 }
 
                 // Sort the list
-                if(foreground[foregroundAt0 + 1].position.x < 0)
-                {   
+                if (foreground[foregroundAt0 + 1].position.x < 0)
+                {
                     foreground[0].position = foreground[foreground.Length - 1].position + shiftForegroundBy;
                     ReSortArray<Rigidbody2D>(foreground);
                 }
             }
 
-            if(updateObstacles == true)
+            if (updateObstacles == true)
             {
-                for(index = 0; index < obstacle.Length; ++index)
+                for (index = 0; index < obstacle.Length; ++index)
                 {
                     cache = obstacle[index].position;
                     cache.x += left.x;
                     obstacle[index].MovePosition(cache);
                 }
 
-                if((cycleObstacles == true) && (obstacle[obstacleAt0 + 1].position.x < 0))
+                if ((cycleObstacles == true) && (obstacle[obstacleAt0 + 1].position.x < 0))
                 {
-                    if(obstacle[0].gameObject.activeSelf == true)
+                    if (obstacle[0].gameObject.activeSelf == true)
                     {
                         obstacle[0].position = PositionObstacle(obstacle[obstacle.Length - 1].position + shiftObstacleBy);
                     }
@@ -179,15 +179,15 @@ public class ScrollBackground : MonoBehaviour
                     }
                     ReSortArray<Rigidbody2D>(obstacle);
                     obstacle[obstacle.Length - 1].gameObject.SetActive(true);
-                    if((diamonds != null) && (diamonds.Length > 0))
+                    if ((diamonds != null) && (diamonds.Length > 0))
                     {
                         PositionDiamond(obstacle[obstacle.Length - 1].position);
                     }
                 }
 
-                if((diamonds != null) && (diamonds.Length > 0))
+                if ((diamonds != null) && (diamonds.Length > 0))
                 {
-                    for(index = 0; index < diamonds.Length; ++index)
+                    for (index = 0; index < diamonds.Length; ++index)
                     {
                         cache = diamonds[index].Body.position;
                         cache.x += left.x;
@@ -198,15 +198,15 @@ public class ScrollBackground : MonoBehaviour
         }
     }
 
-	void ReSortArray<T>(T[] originalList)
-	{
-		T backup = originalList[0];
-		for(index = 1; index < originalList.Length; ++index)
-		{
-			originalList[index - 1] = originalList[index];
-		}
-		originalList[originalList.Length - 1] = backup;
-	}
+    void ReSortArray<T>(T[] originalList)
+    {
+        T backup = originalList[0];
+        for (index = 1; index < originalList.Length; ++index)
+        {
+            originalList[index - 1] = originalList[index];
+        }
+        originalList[originalList.Length - 1] = backup;
+    }
 
     Vector2 PositionObstacle(Vector2 position)
     {
@@ -248,7 +248,7 @@ public class ScrollBackground : MonoBehaviour
 
     void PositionDiamond(Vector2 position)
     {
-        if((diamondPlacement % interval) == 0)
+        if ((diamondPlacement % interval) == 0)
         {
             // Reset the diamond's condition
             diamonds[diamondsIndex].gameObject.SetActive(true);
@@ -261,7 +261,7 @@ public class ScrollBackground : MonoBehaviour
 
             // Shift to the next diamond
             ++diamondsIndex;
-            if(diamondsIndex >= diamonds.Length)
+            if (diamondsIndex >= diamonds.Length)
             {
                 diamondsIndex = 0;
             }
